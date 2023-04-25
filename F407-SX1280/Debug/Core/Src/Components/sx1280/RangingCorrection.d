c37f22e93bd45f8ebfa32e7ca0a4479594394f5b
Enzo Frese <enzo.frese.g@gmail.com>
2023-04-25 09:51:04 -0300

    Simple communication working

---
+++ F407-SX1280/Debug/Core/Src/Components/sx1280/RangingCorrection.d
@@ -0,0 +1,119 @@
+Core/Src/Components/sx1280/RangingCorrection.o: \
+ ../Core/Src/Components/sx1280/RangingCorrection.cpp \
+ ../Core/Src/Components/sx1280/RangingCorrection.h \
+ ../Core/Src/Components/sx1280/sx1280.h \
+ ../Core/Src/Components/sx1280/radio.h ../Core/Inc/main.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrection_defines.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW0400.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW0800.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW1600.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW1600.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW1600.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW1600.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW1600.h \
+ ../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW1600.h
+../Core/Src/Components/sx1280/RangingCorrection.h:
+../Core/Src/Components/sx1280/sx1280.h:
+../Core/Src/Components/sx1280/radio.h:
+../Core/Inc/main.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Core/Inc/stm32f4xx_hal_conf.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrection_defines.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW0400.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW0800.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF5BW1600.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF6BW1600.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF7BW1600.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF8BW1600.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF9BW1600.h:
+../Core/Src/Components/sx1280/rangingCorrection/rangingCorrectionSF10BW1600.h:
